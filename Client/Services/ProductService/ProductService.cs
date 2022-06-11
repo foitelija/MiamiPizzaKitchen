02954a1cbf30f7d3fed6f1832c8d3047bd03a41b
foitelija <foitelija777@gmail.com>
2022-06-11 18:48:02 +0300

    feat: Add the productservice to the client 28.004

---
+++ Client/Services/ProductService/ProductService.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace BlazorMiamiPizza.Client.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _http;
+
+        public ProductService(HttpClient http)
+        {
+            _http = http;
+        }
+        //храним уже полученную информацию с контроллеров сервера.
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public async Task GetProducts()
+        {
+            var result = 
+                await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
+            if (result != null && result.Data != null)
+                Products = result.Data;
+        }
+    }
+}

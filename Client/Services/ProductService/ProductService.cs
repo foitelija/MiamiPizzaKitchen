8a35fe2fc4dccf0f74bdcd1c38d885bfd0ebe0f7
foitelija <foitelija777@gmail.com>
2022-06-16 14:19:51 +0300

    feat:  Get a Single Product from the Client side 34.004

--- Client/Services/ProductService/ProductService.cs
+++ Client/Services/ProductService/ProductService.cs
@@ -13,6 +13,12 @@ namespace BlazorMiamiPizza.Client.Services.ProductService
         //храним уже полученную информацию с контроллеров сервера.
         public List<Product> Products { get; set; } = new List<Product>();
 
+        public async Task<ServiceResponse<Product>> GetProduct(int productId)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<Product>>($"api/product/{productId}");
+            return result;
+        }
+
         public async Task GetProducts()
         {
             var result = 

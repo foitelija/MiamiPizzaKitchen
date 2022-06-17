523b6e3d529919f9b1349f3cfeeeb439dfc5edb7
foitelija <foitelija777@gmail.com>
2022-06-17 13:53:02 +0300

    feat: Get Products by Category on the Client Side 43.013

--- Server/Controllers/ProductController.cs
+++ Server/Controllers/ProductController.cs
@@ -28,7 +28,7 @@ namespace BlazorMiamiPizza.Server.Controllers
             var result = await _productService.GetProductAsync(productId);
             return Ok(result);
         }
-        [HttpGet("category/{categoryUrl")]
+        [HttpGet("category/{categoryUrl}")]
         public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProductsByCategory(string categoryUrl)
         {
             var result = await _productService.GetProductsByCategory(categoryUrl);

d890520efbeaa451a212eb3c384755610e4b53b0
foitelija <foitelija777@gmail.com>
2022-06-20 16:43:26 +0300

    feat: Add Search Method on Client Side 54.024

--- Client/Services/ProductService/ProductService.cs
+++ Client/Services/ProductService/ProductService.cs
@@ -12,6 +12,7 @@ namespace BlazorMiamiPizza.Client.Services.ProductService
         }
         //храним уже полученную информацию с контроллеров сервера.
         public List<Product> Products { get; set; } = new List<Product>();
+        public string Message { get; set; } = "Загрузка продуктов...";
 
         public event Action ProductsChanged;
 
@@ -32,5 +33,24 @@ namespace BlazorMiamiPizza.Client.Services.ProductService
             ProductsChanged.Invoke();
         }
 
+        public async Task<List<string>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchsuggestions/{searchText}");
+            return result.Data;
+        }
+
+        public async Task SearchProducts(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+            if(result != null && result.Data != null)
+            {
+                Products = result.Data;
+            }
+            if(Products.Count == 0)
+            {
+                Message = "Продуктов нет.";
+            }
+            ProductsChanged.Invoke();
+        }
     }
 }

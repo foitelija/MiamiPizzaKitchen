7ae688e0a7833de9bf37a718838f158286d25a99
foitelija <foitelija777@gmail.com>
2022-06-23 16:29:53 +0300

    feat: Cart Beginning 67.037

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,7 @@ global using BlazorMiamiPizza.Shared;
 global using System.Net.Http.Json;
 global using BlazorMiamiPizza.Client.Services.ProductService;
 global using BlazorMiamiPizza.Client.Services.CategoryService;
+using Blazored.LocalStorage;
 using BlazorMiamiPizza.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -10,6 +11,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();

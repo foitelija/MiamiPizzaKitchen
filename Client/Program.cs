6024683a797daa2f01d562287e8bcda431368835
foitelija <foitelija777@gmail.com>
2022-06-23 16:37:56 +0300

    feat: Implement CartService on Client Side 69.039

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,6 +6,7 @@ using Blazored.LocalStorage;
 using BlazorMiamiPizza.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorMiamiPizza.Client.Services.CartService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -15,5 +16,6 @@ builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICartService, CartService>();
 
 await builder.Build().RunAsync();

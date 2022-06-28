32867506272712a596aae4e5315538facc2af2b4
foitelija <foitelija777@gmail.com>
2022-06-28 15:25:05 +0300

    feat: Add User_db model 88.008

--- Server/Data/DataContext.cs
+++ Server/Data/DataContext.cs
@@ -146,11 +146,10 @@
                 );
         }
 
-
-
         public DbSet <Product> Products { get; set; }
         public DbSet <Category> Categories{ get; set; }
         public DbSet <ProductType> ProductTypes { get; set; }
         public DbSet <ProductVariant> ProductVariants { get; set; }
+        public DbSet <User> Users { get; set; }
     }
 }

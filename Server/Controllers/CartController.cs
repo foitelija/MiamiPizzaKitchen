21e01a6b06f863662e1a3793613f6e3a4db0b2ff
foitelija <foitelija777@gmail.com>
2022-07-12 13:38:06 +0300

    feat: Store CartItems in  DB 119.004

--- Server/Controllers/CartController.cs
+++ Server/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿using BlazorMiamiPizza.Server.Services.CartService;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BlazorMiamiPizza.Server.Controllers
 {
@@ -19,6 +20,14 @@ namespace BlazorMiamiPizza.Server.Controllers
         public async Task<ActionResult<ServiceResponse<List<CartProductResponse>>>> GetCartProducts(List<CartItem> cartItems)
         {
             var result = await _cartService.GetCartProducts(cartItems);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<List<CartProductResponse>>>> StoreCartItems(List<CartItem> cartItems)
+        {
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var result = await _cartService.StoreCartItems(cartItems, userId);
             return Ok(result);
         }
     }

f2c8c9b75168d8609a1d1d75886a3949e3cc2ad6
foitelija <foitelija777@gmail.com>
2022-07-19 13:05:43 +0300

    refactor: Add more prop for Category model 159.004

---
+++ Server/Migrations/20220719100323_CategoryFlags.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorMiamiPizza.Server.Migrations
+{
+    public partial class CategoryFlags : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isDeleted",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isVisible",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "isVisible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "isVisible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "isVisible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "isVisible",
+                value: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isDeleted",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "isVisible",
+                table: "Categories");
+        }
+    }
+}
--- Shared/Category.cs
+++ Shared/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,12 @@ namespace BlazorMiamiPizza.Shared
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Url { get; set; } = string.Empty;
+
+        public bool isVisible { get; set; } = true;
+        public bool isDeleted { get; set; } = false;
+        [NotMapped]
+        public bool isEditing { get; set; } = false;
+        [NotMapped]
+        public bool isNew { get; set; } = false;
     }
 }

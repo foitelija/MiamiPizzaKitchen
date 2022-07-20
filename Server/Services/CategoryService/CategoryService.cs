de2fa96ef7d21c5f10357b9ea55ab51fc17d0887
foitelija <foitelija777@gmail.com>
2022-07-20 12:23:41 +0300

    feat: CRUD for Categories on Server 160.005

--- Server/Services/CategoryService/CategoryService.cs
+++ Server/Services/CategoryService/CategoryService.cs
@@ -9,13 +9,79 @@
             _context = context;
         }
 
-        public async Task<ServiceResponse<List<Category>>> GetCategories()
+        public async Task<ServiceResponse<List<Category>>> AddCategory(Category category)
         {
-            var categories = await _context.Categories.ToListAsync();
+            category.isEditing = category.isNew = false;
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            return await GetAdminCategories();
+        }
+
+        public async Task<ServiceResponse<List<Category>>> DeleteCategory(int id)
+        {
+            Category category = await GetCategoryById(id);
+            if(category == null)
+            {
+                return new ServiceResponse<List<Category>>
+                {
+                    Success = false,
+                    Message = "Категория не найдена."
+                };
+            }
+
+            category.isDeleted = true;
+            await _context.SaveChangesAsync();
+
+            return await GetAdminCategories();
+        }
+
+        private async Task<Category> GetCategoryById(int id)
+        {
+            return await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<ServiceResponse<List<Category>>> GetAdminCategories()
+        {
+            var categories = await _context.Categories
+                .Where(c => !c.isDeleted)
+                .ToListAsync();
             return new ServiceResponse<List<Category>>
             {
                 Data = categories,
             };
+        }
+
+        public async Task<ServiceResponse<List<Category>>> GetCategories()
+        {
+            var categories = await _context.Categories
+                .Where(c=> !c.isDeleted && c.isVisible)
+                .ToListAsync();
+            return new ServiceResponse<List<Category>>
+            {
+                Data = categories,
+            };
+        }
+
+        public async Task<ServiceResponse<List<Category>>> UpdateCategory(Category category)
+        {
+            var dbCategory = await GetCategoryById(category.Id);
+            if(dbCategory == null)
+            {
+                return new ServiceResponse<List<Category>>
+                {
+                    Success = false,
+                    Message = "Категория не найдена."
+                };
+            }
+
+            dbCategory.Name = category.Name;
+            dbCategory.Url = category.Url;
+            dbCategory.isVisible = category.isVisible;
+
+
+            await _context.SaveChangesAsync();
+
+            return await GetAdminCategories();
         }
     }
 }

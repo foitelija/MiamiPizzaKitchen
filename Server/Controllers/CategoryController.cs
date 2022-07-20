c2668ae462ba22198f93f217949cfde183b81055
foitelija <foitelija777@gmail.com>
2022-07-20 12:34:16 +0300

    feat: CRUD for Categories on Client 161.006

--- Server/Controllers/CategoryController.cs
+++ Server/Controllers/CategoryController.cs
@@ -30,7 +30,7 @@ namespace BlazorMiamiPizza.Server.Controllers
         }
 
 
-        [HttpDelete("admin"), Authorize(Roles = "Admin")]
+        [HttpDelete("admin/{id}"), Authorize(Roles = "Admin")]
         public async Task<ActionResult<ServiceResponse<List<Category>>>> DeleteCategory(int id)
         {
             var result = await _categoryService.DeleteCategory(id);

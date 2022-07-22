3f02fbd2cd1f64159f53f42e33425c071f9bffc7
foitelija <foitelija777@gmail.com>
2022-07-22 19:56:30 +0300

    feat: Add & Update ProductTypes on Server 167.012

--- Server/Services/ProductTypeService/ProductTypeService.cs
+++ Server/Services/ProductTypeService/ProductTypeService.cs
@@ -9,6 +9,14 @@
             _context = context;
         }
 
+        public async Task<ServiceResponse<List<ProductType>>> AddProductTypes(ProductType productType)
+        {
+            _context.ProductTypes.Add(productType);
+            await _context.SaveChangesAsync();
+
+            return await GetProductTypes();
+        }
+
         public async Task<ServiceResponse<List<ProductType>>> GetProductTypes()
         {
             var productTypes = await _context.ProductTypes.ToListAsync();
@@ -16,6 +24,24 @@
             {
                 Data = productTypes
             };
+        }
+
+        public async Task<ServiceResponse<List<ProductType>>> UpdateProductTypes(ProductType productType)
+        {
+            var dbProductType = await _context.ProductTypes.FindAsync(productType.Id);
+            if(dbProductType == null)
+            {
+                return new ServiceResponse<List<ProductType>>
+                {
+                    Success = false,
+                    Message = "Типы продуктов не найдены."
+                };
+            }
+
+            dbProductType.Name = productType.Name;
+            await _context.SaveChangesAsync();
+
+            return await GetProductTypes();
         }
     }
 }
